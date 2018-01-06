126dc97eb4e6163a1e867b58631272fdc50c9097
Javier <javierfz1980@gmail.com>
2018-01-06 00:11:50 -0300

    Students grid view

--- client/src/app/content/students/students.component.ts
+++ client/src/app/content/students/students.component.ts
@@ -1,14 +1,26 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {Observable} from "rxjs/Observable";
+import 'rxjs/add/operator/filter';
+import {Student} from "../../common/models/student";
+import {ContentService} from "../../common/services/content.service";
+import {globalProperties} from "../../../environments/properties";
 
 @Component({
   selector: "gl-alumnos",
   templateUrl: "./students.component.html"
 })
-export class StudentsComponent {
+export class StudentsComponent implements OnInit {
 
-  private readonly title: string = "All Students";
+  students: Observable<Student[]>;
+  title: string = "All Students";
 
-  constructor() {}
+  constructor(private contentService: ContentService) {}
 
+  ngOnInit() {
+
+    this.students = this.contentService
+      .getContent<Student[]>(globalProperties.studentsPath)
+      .catch(error => Observable.throw(error));
+  }
 
 }
