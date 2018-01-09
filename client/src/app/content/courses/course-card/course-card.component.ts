78ae62330353b90258a05473fd3b51952da3cce1
Javier <javierfz1980@gmail.com>
2018-01-09 15:41:44 -0300

    Add Courses Delete

--- client/src/app/content/courses/course-card/course-card.component.ts
+++ client/src/app/content/courses/course-card/course-card.component.ts
@@ -1,23 +1,32 @@
-import {Component, Input} from "@angular/core";
+import {Component, EventEmitter, Input, Output} from "@angular/core";
 import {Course} from "../../../models/course";
 import {Router} from "@angular/router";
 
 @Component({
   selector: "gl-course-card",
-  templateUrl: "./course-card.component.html"
+  templateUrl: "./course-card.component.html",
+  styleUrls: ['./course-card.component.css']
 })
 export class CourseCardComponent {
 
   @Input()
   course: Course;
 
-  constructor(private router: Router) {}
+  @Input()
+  isAdministrator: boolean;
+
+  @Output('onDelete')
+  deleteEvent: EventEmitter<Course> = new EventEmitter<Course>();
 
   /**
    * Navigates to a single Course
    */
   goToSingleCourse() {
     //this.router.navigate(["/" + routePaths.post + "/" + this.post.id]);
+  }
+
+  delete(course: Course) {
+    this.deleteEvent.emit(course);
   }
 
 }
--- client/src/app/content/courses/courses.component.ts
+++ client/src/app/content/courses/courses.component.ts
@@ -1,31 +1,80 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnDestroy, OnInit, ViewChild} from "@angular/core";
 import {Observable} from "rxjs/Observable";
 import "rxjs/add/operator/filter";
 import {Course} from "../../models/course";
 import {ContentService} from "../../core/providers/services/content.service";
 import {globalProperties} from "../../../environments/properties";
+import {AuthService} from "../../core/providers/services/auth.service";
+import {
+  ConfirmationData,
+  ConfirmationModalComponent
+} from "../commons/confirmation-modal/confirmation-modal.component";
+import {MessageResponse} from "../../models/api/message-response";
+import {Subscription} from "rxjs/Subscription";
+import {ContentAlert} from "../commons/content-alert/content-alert.component";
 
 @Component({
   selector: "gl-cursos",
   templateUrl: "./courses.component.html"
 })
-export class CoursesComponent implements OnInit {
+export class CoursesComponent implements OnInit, OnDestroy {
 
-  courses: Observable<Course[]>;
+  @ViewChild("confirmModal") confirmModal: ConfirmationModalComponent;
+
   title = "All Courses";
+  courses: Observable<Course[]>;
+  isAdministrator: boolean;
+  modalData: ConfirmationData;
+  alert: ContentAlert;
+  subscription: Subscription;
+  busy: boolean = false;
 
-  constructor(private contentService: ContentService) {}
+  private coursesPath: string = globalProperties.coursesPath;
+
+  constructor(private contentService: ContentService,
+              private authService: AuthService) {}
 
   ngOnInit() {
+    this.fetchContent();
+    this.isAdministrator = this.authService.isAdministrator();
 
+  }
+
+  fetchContent() {
     this.courses = this.contentService
-      .getContent<Course[]>(globalProperties.coursesPath)
-      .catch(error => Observable.throw(error))
+      .getContent<Course[]>(this.coursesPath)
+      .catch(error => Observable.throw(error));
+  }
 
+  deleteCourse(course: Course) {
+    this.modalData = {
+      type: "delete",
+      title: "Delete",
+      text: "Are you sure you want to delete the element ?",
+      action: this.delete(course)
+    };
+    this.confirmModal.open();
+  }
 
-      /*.select("entities", "courses")
-      .filter(courses => courses !== undefined)
-      .do(state => console.log("courses state: ", state));*/
+  private delete(course: Course) {
+    return () => {
+      this.busy = true;
+      this.subscription = this.contentService.deleteContent<MessageResponse>(this.coursesPath, course.id)
+        .map((response: MessageResponse) => {
+          this.alert = {type: "success", message: response.message, time: 3000};
+          this.busy = false;
+          this.fetchContent();
+        })
+        .catch(error => {
+          this.busy = false;
+          this.alert = {type: "danger", message: error};
+          return Observable.throw(error);
+        })
+        .subscribe();
+    }
+  }
 
+  ngOnDestroy() {
+    if (this.subscription) this.subscription.unsubscribe();
   }
 }
