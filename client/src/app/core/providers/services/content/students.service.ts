5fdf746ac39c18868d0d8dd048a3974ba6aa0f9a
javier.fernandez <javier.fernandez@globallogic.com>
2018-01-18 16:15:23 -0300

    StudentsService code move and clean up

--- client/src/app/core/providers/services/content/students.service.ts
+++ client/src/app/core/providers/services/content/students.service.ts
@@ -43,35 +43,34 @@ export class StudentsService {
       })
   }
 
-  createStudent(student: Student, profile: Profile, courses: string[]): Observable<ContentAlert> {
-    profile.birthday = new Date(profile.birthday).toString();
-    return Observable.forkJoin(
-        this.contentService.postContent<Student>(this.path, student),
-        this.contentService.postContent<Profile>(this.profilesPath, profile)
-      )
-      .switchMap(([newStudent, newProfile]) => {
-        return Observable.forkJoin(
-            this.addStudentToCourses(newStudent.id, courses),
-            this.contentService.patchContent<Student>(this.path, newStudent.id, ({
-                profile_id: newProfile.id,
-                courses: student.courses
-              }))
-          )
-          .map(() => (<ContentAlert>{
-            type: "success",
-            message: "Student created",
-            time: 3000
-          }))
-          .catch((error: any) => Observable.of(<ContentAlert>{
-            type: "danger",
-            message: `Error creating student: ${error.message}`
-          }));
-      })
+  updateStudentsForCourses(studentId: string, coursesToBeRemoved: string[], coursesToBeAdded: string[]): Observable<Course[]> {
+    return this.delteStudentFromCourses(studentId, coursesToBeRemoved)
+      .switchMap(() => this.addStudentToCourses(studentId, coursesToBeAdded))
   }
 
-  updateStudentsForCourses(studentId: string, coursesToBeRemoved: string[], coursesToBeAdded: string[]): Observable<Course[]> {
-    return this.removeStudentFromCourses(studentId, coursesToBeRemoved)
-      .switchMap(() => this.addStudentToCourses(studentId, coursesToBeAdded))
+  updateStudentInfo(student:Student, profile: Profile, coursesToBeRemoved: string[], coursesToBeAdded: string[]): Observable<ContentAlert> {
+    const infoId: string = student.id;
+    const profileId: string = profile.id;
+
+    delete student.id;
+    delete profile.id;
+
+    profile.birthday = new Date(profile.birthday).toString();
+
+    return Observable.forkJoin(
+      this.contentService.patchContent<Student>(this.path, infoId, student),
+      this.contentService.patchContent<Profile>(this.profilesPath, profileId, profile),
+      this.updateStudentsForCourses(infoId, coursesToBeRemoved, coursesToBeAdded)
+    )
+      .map(() => (<ContentAlert>{
+        type: "success",
+        message: "Student info updated",
+        time: 3000
+      }))
+      .catch((error: any) => Observable.of(<ContentAlert>{
+        type: "danger",
+        message: `Error updating info: ${error.message}`
+      }))
   }
 
   addStudentToCourses(studentId: string, courses: string[]): Observable<Course[]> {
@@ -86,7 +85,33 @@ export class StudentsService {
       .toArray();
   }
 
-  removeStudentFromCourses(studentId: string, courses: string[]): Observable<Course[]> {
+  createStudent(student: Student, profile: Profile, courses: string[]): Observable<ContentAlert> {
+    profile.birthday = new Date(profile.birthday).toString();
+    return Observable.forkJoin(
+      this.contentService.postContent<Student>(this.path, student),
+      this.contentService.postContent<Profile>(this.profilesPath, profile)
+    )
+      .switchMap(([newStudent, newProfile]) => {
+        return Observable.forkJoin(
+          this.addStudentToCourses(newStudent.id, courses),
+          this.contentService.patchContent<Student>(this.path, newStudent.id, ({
+            profile_id: newProfile.id,
+            courses: student.courses
+          }))
+        )
+          .map(() => (<ContentAlert>{
+            type: "success",
+            message: "Student created",
+            time: 3000
+          }))
+          .catch((error: any) => Observable.of(<ContentAlert>{
+            type: "danger",
+            message: `Error creating student: ${error.message}`
+          }));
+      })
+  }
+
+  delteStudentFromCourses(studentId: string, courses: string[]): Observable<Course[]> {
     if (courses.length === 0) return Observable.of([]);
     return Observable.from(courses)
       .mergeMap((courseId: string) => this.contentService
@@ -99,36 +124,11 @@ export class StudentsService {
       .toArray();
   }
 
-  updateStudentInfo(student:Student, profile: Profile, coursesToBeRemoved: string[], coursesToBeAdded: string[]): Observable<ContentAlert> {
-    const infoId: string = student.id;
-    const profileId: string = profile.id;
-
-    delete student.id;
-    delete profile.id;
-
-    profile.birthday = new Date(profile.birthday).toString();
-
-    return Observable.forkJoin(
-        this.contentService.patchContent<Student>(this.path, infoId, student),
-        this.contentService.patchContent<Profile>(this.profilesPath, profileId, profile),
-        this.updateStudentsForCourses(infoId, coursesToBeRemoved, coursesToBeAdded)
-      )
-      .map(() => (<ContentAlert>{
-        type: "success",
-        message: "Student info updated",
-        time: 3000
-      }))
-      .catch((error: any) => Observable.of(<ContentAlert>{
-        type: "danger",
-        message: `Error updating info: ${error.message}`
-      }))
-  }
-
   deleteStudent(student: Student): Observable<ContentAlert> {
     return Observable.forkJoin(
         this.contentService.deleteContent<MessageResponse>(this.profilesPath, student.profile_id),
         this.contentService.deleteContent<MessageResponse>(this.path, student.id),
-        this.removeStudentFromCourses(student.id, student.courses)
+        this.delteStudentFromCourses(student.id, student.courses)
       )
       .map(([deleteProfile, deleteStudent]) => (<ContentAlert>{
         type: "success",
