8218b78e1ac5a216d3ccee409c0b538036a564fa
javier.fernandez <javier.fernandez@globallogic.com>
2018-01-22 01:33:25 -0300

    Adds new Helper to create and return a copy from any Object

--- client/src/app/helpers/helpers.ts
+++ client/src/app/helpers/helpers.ts
@@ -2,3 +2,7 @@ export function getDifferencesBetween<T>(origin: T[], destiny: T[]): T[] {
   return origin.filter((originItem: T) => destiny.indexOf(originItem) < 0)
 }
 
+export function objectCopy<T>(source: T): T {
+  return Object.assign({}, source);
+}
+
