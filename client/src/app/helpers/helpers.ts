8f49a2ed1cc6cfd86ab80e67321c1eada0dbd5e3
javier.fernandez <javier.fernandez@globallogic.com>
2018-01-24 21:06:22 -0300

    new helper random between range

--- client/src/app/helpers/helpers.ts
+++ client/src/app/helpers/helpers.ts
@@ -6,3 +6,7 @@ export function objectCopy<T>(source: T): T {
   return Object.assign({}, source);
 }
 
+export function randomBetween(min: number, max: number) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
