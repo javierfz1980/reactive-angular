1250f21bd29d0b4ef9a177d6909ad413e110e934
javier.fernandez <javier.fernandez@globallogic.com>
2018-01-27 03:52:02 -0300

    Make CreateTeacher component extends from AbstractClass BasicSingleEditorWithList in order to reuse implementation.

--- client/src/app/content/teachers/create-teacher/create-teacher.component.ts
+++ client/src/app/content/teachers/create-teacher/create-teacher.component.ts
@@ -4,7 +4,7 @@ import {
   ConfirmationModalData,
   ConfirmationModalComponent
 } from "../../../commons/confirmation-modal/confirmation-modal.component";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {InfoProfileData} from "../../commons/forms/info/info-profile/info-profile-form.component";
 import {appRoutePaths} from "../../../app-routing.module";
 import {ContentService} from "../../../core/providers/services/content/content.service";
@@ -14,59 +14,52 @@ import {BehaviorSubject} from "rxjs/BehaviorSubject";
 import {Course} from "../../../models/content/course";
 import {StoreData} from "../../../core/providers/services/content/basic-content.service";
 import {Teacher} from "../../../models/content/teacher";
+import {BasicInfoProfileList} from "../../commons/abstarct-clases/basic-info-profile-list";
 
 @Component({
   selector: "gl-create-teacher",
   templateUrl: "create-teacher.component.html"
 })
-export class CreateTeacherComponent implements OnDestroy{
+export class CreateTeacherComponent extends BasicInfoProfileList<InfoProfileData, CoursesListFormComponent, Course> implements OnDestroy{
 
   @ViewChild("confirmModal")
   confirmModal: ConfirmationModalComponent;
 
-  @ViewChild("courses")
-  teacherCourses: CoursesListFormComponent;
+  @ViewChild("listForm")
+  listForm: CoursesListFormComponent;
 
   title: string = "Add new Teacher";
-  modalData: ConfirmationModalData;
-  isAdministrator: boolean;
-
-  listFormSource: Observable<StoreData<Course>>;
-  listFormMarked: Observable<string[]>;
-  isEditMode: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
-
   private isAlive: boolean = true;
 
-  constructor(private contentService: ContentService,
-              private authService: AuthService,
-              private router: Router) {
-    this.isAdministrator = this.authService.isAdministrator();
+  constructor(protected authService: AuthService,
+              private contentService: ContentService,
+              private router: Router,
+              protected route: ActivatedRoute) {
+    super(authService, route);
+  }
 
+  ngOnInit() {
+    super.ngOnInit();
     this.listFormSource = this.contentService.getCourses();
     this.listFormMarked = Observable.of([]);
     this.isEditMode.next((this.isAdministrator && true));
   }
 
   create(data: InfoProfileData) {
-    this.modalData = {
-      type: "confirm",
-      title: "Create",
-      text: "Are you sure you want to create this new Teacher ?",
-      action: () => {
-        this.modalData.title = "Creating";
-        this.modalData.isBusy = true;
-        this.contentService
-          .createTeacher(data.info, data.profile, this.teacherCourses.getSelecteds())
-          .takeWhile(() => this.isAlive)
-          .subscribe(
-            () => {
-              this.modalData.isBusy = false;
-              this.confirmModal.close();
-              this.router.navigate([appRoutePaths.teachers.path])
-            });
-      }
+    this.action = () => {
+      this.modalData.title = "Creating";
+      this.modalData.isBusy = true;
+      this.contentService
+        .createTeacher(data.info, data.profile, this.listForm.getSelecteds())
+        .takeWhile(() => this.isAlive)
+        .subscribe(
+          () => {
+            this.modalData.isBusy = false;
+            this.confirmModal.close();
+            this.router.navigate([appRoutePaths.teachers.path])
+          });
     };
-    this.confirmModal.open();
+    this.openCreateConfirmation();
   }
 
   ngOnDestroy() {
