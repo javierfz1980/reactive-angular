4bc065b06b13cdc9350eba95a046e4ed53192e95
javier.fernandez <javier.fernandez@globallogic.com>
2018-01-29 15:37:37 -0300

    Change SingleTeacher in order to display teacher info and active courses on a single view stack + some fixes and improvements
    This change will introduce:
    1- A new view of the SingleTeacher in order to display the Teacher info and active courses in a single view.

--- client/src/app/content/teachers/single-teacher/single-teacher.component.ts
+++ client/src/app/content/teachers/single-teacher/single-teacher.component.ts
@@ -1,39 +1,36 @@
 import {Component, OnDestroy, OnInit, ViewChild} from "@angular/core";
 import {ActivatedRoute, Params, Router} from "@angular/router";
-import {CoursesListFormComponent} from "../../commons/forms/lists/courses-list/courses-list-form.component";
 import {AuthService} from "../../../core/providers/services/auth.service";
 import {Observable} from "rxjs/Observable";
 import {
-  ConfirmationModalData,
   ConfirmationModalComponent
 } from "../../../commons/confirmation-modal/confirmation-modal.component";
 import {Teacher} from "../../../models/content/teacher";
-import {InfoProfileData} from "../../commons/forms/info/info-profile/info-profile-form.component";
+import {
+  InfoProfileData,
+  InfoProfileFormComponent
+} from "../../commons/forms/info/info-profile/info-profile-form.component";
 import {appRoutePaths} from "../../../app-routing.module";
 import {ContentService} from "../../../core/providers/services/content/content.service";
-import {getDifferencesBetween} from "../../../helpers/helpers";
 import 'rxjs/add/operator/takeWhile';
-import {Alert} from "../../../models/core/alert";
-import {BehaviorSubject} from "rxjs/BehaviorSubject";
 import {Course} from "../../../models/content/course";
-import {Student} from "../../../models/content/student";
-import {BasicInfoProfileList} from "../../commons/abstarct-clases/basic-info-profile-list";
-import {StudentsListFormComponent} from "../../commons/forms/lists/students-list/students-list-form.component";
+import {BasicInfoList} from "../../commons/abstarct-clases/basic-info-list";
 
 @Component({
   selector: "gl-single-teacher",
   templateUrl: "./single-teacher.component.html"
 })
-export class SingleTeacherComponent extends BasicInfoProfileList<InfoProfileData, CoursesListFormComponent, Course>
+export class SingleTeacherComponent extends BasicInfoList<InfoProfileData, InfoProfileFormComponent, Course>
                                     implements OnInit, OnDestroy {
 
   @ViewChild("confirmModal")
   confirmModal: ConfirmationModalComponent;
 
-  @ViewChild("listForm")
-  listForm: CoursesListFormComponent;
+  @ViewChild("infoForm")
+  infoForm: InfoProfileFormComponent;
 
-  isAlive: boolean = true;
+  action: () => void;
+  private isAlive: boolean = true;
 
   constructor(private router: Router,
               private contentService: ContentService,
@@ -68,7 +65,7 @@ export class SingleTeacherComponent extends BasicInfoProfileList<InfoProfileData
       });
 
     this.listFormSource = this.contentService
-      .getCourses();
+      .getCoursesWithTeacher();
 
     this.listFormMarked = this.source
       .map((data: InfoProfileData) => data.info)
@@ -104,8 +101,8 @@ export class SingleTeacherComponent extends BasicInfoProfileList<InfoProfileData
             this.modalData.isBusy = false;
             this.confirmModal.close();
           });
-    }
-    super.openUpdateConfirmation(data.info.courses, this.listForm.getSelecteds())
+    };
+    super.openUpdateConfirmation(data.info.courses, this.infoForm.listForm.getSelecteds())
   }
 
   ngOnDestroy() {
