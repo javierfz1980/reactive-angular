6ef62b174729c1ab5286f3dc7f79c5cb8844457a
javier.fernandez <javier.fernandez@globallogic.com>
2018-02-02 11:53:49 -0300

    add needed import after upgrade CLI

--- client/src/app/core/providers/services/auth.service.ts
+++ client/src/app/core/providers/services/auth.service.ts
@@ -11,6 +11,7 @@ import {getFakedDelay} from "../../../helpers/helpers";
 import "rxjs/add/operator/catch";
 import "rxjs/add/operator/do";
 import "rxjs/add/operator/switchMap";
+import 'rxjs/add/operator/delay';
 import 'rxjs/add/observable/throw';
 
 @Injectable()
